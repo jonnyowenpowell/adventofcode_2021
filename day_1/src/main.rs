531b2dd92f97865249ab55ac23e54f567f8ff794
Jonny Powell <jonnyowenpowell@gmail.com>
2021-12-02 19:53:28 +0000

    Parsed input for day_1

--- day_1/src/main.rs
+++ day_1/src/main.rs
@@ -1,5 +1,10 @@
 fn main() {
     let input = include_str!("../input.txt");
+    let readings: Vec<i32> = input
+        .split("\n")
+        .filter(|l| !l.is_empty())
+        .map(|l| l.parse().unwrap())
+        .collect();
 
-    println!("Input:\n{}", input);
+    println!("Input:\n{:?}", readings);
 }
