7055874dd981f27fd1cc8e887226f5f543add106
Jonny Powell <jonnyowenpowell@gmail.com>
2021-12-03 11:23:52 +0000

    Implemented counting bits for day_3

--- day_3/src/main.rs
+++ day_3/src/main.rs
@@ -1,5 +1,22 @@
 fn main() {
     let input = include_str!("../input.txt");
+    let lines: Vec<_> = input.split("\n").filter(|l| !l.is_empty()).collect();
 
-    println!("Input:\n{}", input);
+    let bit_length = lines[0].len();
+    let gamma_bit_counts = vec![0 as isize; bit_length];
+
+    let gamma_bit_counts = lines.iter().fold(gamma_bit_counts, |bits, l| {
+        let count_delta: Vec<isize> = l
+            .as_bytes()
+            .iter()
+            .map(|c| if c == &('0' as u8) { -1 } else { 1 })
+            .collect();
+
+        bits.iter()
+            .enumerate()
+            .map(|(i, c)| c + count_delta[i])
+            .collect()
+    });
+
+    println!("Bit counts:\n{:?}", gamma_bit_counts);
 }
