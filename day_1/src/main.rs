088dd63301c8e05f282571ecf5eb6bf78d774d15
Jonny Powell <jonnyowenpowell@gmail.com>
2021-12-05 16:15:49 +0000

    Refactored day_1 solution and added benchmarks

--- day_1/src/main.rs
+++ day_1/src/main.rs
@@ -1,28 +1,82 @@
-fn main() {
-    let input = include_str!("../input.txt");
-    let readings: Vec<i32> = input
-        .split("\n")
-        .filter(|l| !l.is_empty())
-        .map(|l| l.parse().unwrap())
-        .collect();
+#![feature(test)]
 
+fn main() {
+    let mut input = parse_input();
+
+    let reading_increases = count_increases(&input);
+    println!("Reading Increases:\n{}", reading_increases);
+
+    let window_increases = count_window_increases(&mut input);
+    println!("Window Increases:\n{}", window_increases);
+}
+
+fn parse_input() -> Vec<u32> {
+    include_str!("../input.txt")
+        .lines()
+        .map(|l| l.parse().unwrap())
+        .collect()
+}
+
+fn count_increases(input: &Vec<u32>) -> u32 {
     let mut reading_increases = 0;
-    for i in 1..readings.len() {
-        if readings[i] > readings[i - 1] {
+    for i in 1..input.len() {
+        if input[i] > input[i - 1] {
             reading_increases += 1;
         }
     }
+    reading_increases
+}
 
-    println!("Reading Increases:\n{}", reading_increases);
+fn calculate_cumulative_sums(input: &mut Vec<u32>) -> &mut Vec<u32> {
+    input.iter_mut().fold(0, |acc, x| {
+        *x += acc;
+        *x
+    });
+    input
+}
 
-    let mut window_increases = 0;
-    for i in 0..(readings.len() - 3) {
-        let window_a: i32 = readings[i..i + 3].iter().sum();
-        let window_b: i32 = readings[i + 1..i + 4].iter().sum();
-        if window_a < window_b {
-            window_increases += 1;
-        }
+fn count_window_increases(input: &mut Vec<u32>) -> u32 {
+    let cumulative_sums = calculate_cumulative_sums(input);
+    let windows: Vec<u32> = cumulative_sums
+        .iter()
+        .skip(3)
+        .zip(cumulative_sums.iter())
+        .map(|(s0, s1)| s0 - s1)
+        .collect();
+    count_increases(&windows)
+}
+
+extern crate test;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use rand::{distributions::Uniform, Rng};
+    use test::Bencher;
+
+    #[bench]
+    fn bench_calculate_cumulative_sums(b: &mut Bencher) {
+        let mut rng = rand::thread_rng();
+        let range = Uniform::new(100, 5000);
+        let mut input: Vec<u32> = (0..1000).map(|_| rng.sample(&range)).collect();
+
+        b.iter(|| {
+            calculate_cumulative_sums(&mut input);
+        });
     }
 
-    println!("Window Increases:\n{}", window_increases);
+    #[bench]
+    fn bench_count_increases(b: &mut Bencher) {
+        let input = parse_input();
+        b.iter(|| count_increases(&input));
+    }
+
+    #[bench]
+    fn bench_count_window_increases(b: &mut Bencher) {
+        let input = parse_input();
+        b.iter(|| {
+            let mut input = input.to_vec();
+            count_window_increases(&mut input)
+        });
+    }
 }
